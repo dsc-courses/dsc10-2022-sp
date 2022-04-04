432fe34c84dad61f860d8f6a4938179848b26119
Couson <couson@foxmail.com>
2022-04-04 11:45:45 -0700

    upload disc01

---
+++ discussions/01-python_basics/tests/q3_1.py
@@ -0,0 +1,7 @@
+test = {   'name': 'q3_1',
+    'points': None,
+    'suites': [   {   'cases': [{'code': '>>> import math\n>>> math.isclose(bmi, 24.34137521336267)\nTrue', 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
---
+++ discussions/01-python_basics/tests/q3_2.py
@@ -0,0 +1,14 @@
+test = {   'name': 'q3_2',
+    'points': None,
+    'suites': [   {   'cases': [   {'code': '>>> type(part2) != str # Make sure to convert your datatypes!\nFalse', 'hidden': False, 'locked': False},
+                                   {'code': '>>> type(part2) == str\nTrue', 'hidden': False, 'locked': False},
+                                   {   'code': '>>> sentence != "Alex\'s BMI is 24.34137521336267." # Make sure the apostrophe is displayed correctly, there are spaces, and you have a period at the '
+                                               'end!\n'
+                                               'False',
+                                       'hidden': False,
+                                       'locked': False},
+                                   {'code': '>>> sentence == "Alex\'s BMI is 24.34137521336267."\nTrue', 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
