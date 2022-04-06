d7a75ce7a54796aa16b4b63539fa65febc27fcfb
xd00099 <dxiang@ucsd.edu>
2022-04-05 22:53:01 -0700

    add hw2

---
+++ homeworks/02-arrays_dataframes/tests/q3_4.py
@@ -0,0 +1,9 @@
+test = {   'name': 'q3_4',
+    'points': None,
+    'suites': [   {   'cases': [   {   'code': '>>> import numbers\n>>> isinstance(eighth_highest_salary, numbers.Integral) and isinstance(eighth_player_name, str)\nTrue',
+                                       'hidden': False,
+                                       'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
