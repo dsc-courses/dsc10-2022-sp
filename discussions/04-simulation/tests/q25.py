9b77aad97536bfaad94aac725a48b0f933fdbe26
Couson <couson@foxmail.com>
2022-04-25 00:44:41 -0700

    upload disc04

---
+++ discussions/04-simulation/tests/q25.py
@@ -0,0 +1,7 @@
+test = {   'name': 'q25',
+    'points': None,
+    'suites': [   {   'cases': [{'code': '>>> import math\n>>> math.isclose(at_least_2_clubs_prob, 0.367046818727491)\nTrue', 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
---
+++ discussions/04-simulation/tests/q26.py
@@ -0,0 +1,3 @@
+test = {   'name': 'q26',
+    'points': None,
+    'suites': [{'cases': [{'code': '>>> all_clubs_prob == 0.0004951980792316927\nTrue', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
