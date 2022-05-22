2bdfcda0162e5e6dd9362cf3c946a4eb630a1bc3
Couson <couson@foxmail.com>
2022-05-22 01:41:14 -0700

    upload disc08

---
+++ discussions/08-normal_clt/tests/q1_3.py
@@ -0,0 +1,3 @@
+test = {   'name': 'q1_3',
+    'points': None,
+    'suites': [{'cases': [{'code': '>>> cheby_area_pm_3 == 1 - (1/(3**2))\nTrue', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
---
+++ discussions/08-normal_clt/tests/q4_6.py
@@ -0,0 +1,3 @@
+test = {   'name': 'q4_6',
+    'points': None,
+    'suites': [{'cases': [{'code': '>>> approx_prop_dist == 0.257\nFalse', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
